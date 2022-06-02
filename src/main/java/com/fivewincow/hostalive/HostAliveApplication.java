240aca537be838b84a76afc523dbf08314b6c698
fivewincow <fivewincow@gmail.com>
2022-06-02 17:34:12 +0900

    project init

---
+++ src/main/java/com/fivewincow/hostalive/HostAliveApplication.java
@@ -0,0 +1,13 @@
+package com.fivewincow.hostalive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HostAliveApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HostAliveApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/fivewincow/hostalive/HostAliveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fivewincow.hostalive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HostAliveApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
